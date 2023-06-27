9c3e9b2d8548c121bec63526fb0592377c223340
Alberto Ito <95650418+Alberto-Ito@users.noreply.github.com>
2023-06-27 17:57:01 -0300

    Update set-env.js

--- src/set-env.js
+++ src/set-env.js
@@ -1,7 +1,8 @@
-(async () => {
+(() => {
     try {
         process.env['HTTP_PROXY'] = '10.100.18.4:3128';
         process.env['HTTPS_PROXY'] = '10.100.18.4:3128';
+        console.log(process.env['HTTPS_PROXY']);
     } catch (error) {
         core.setFailed(error.message);
     }
